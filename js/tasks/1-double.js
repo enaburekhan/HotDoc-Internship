0d3a8d3c3a9123962fa14e7d15d091ed42aed73b
enaburekhan <ericenaburekhan2018@gmail.com>
2022-03-02 18:19:36 +0100

    Solve double integer

---
+++ js/tasks/1-double.js
@@ -0,0 +1,11 @@
+#! npm test ./tests/1-double.js
+
+/**
+ * Doubles an integer. Not a trick question.
+ * @param {Number} i The integer to double.
+ * @returns {Number} The input doubled
+ */
+export default function doubleInteger(i) {
+//   O(1) time | O(1) space
+    return i * 2;    
+}
